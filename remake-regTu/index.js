8c08d70d9f2f9225d7adf305cc31672b6a02a23e
Pathanin Prateepthinthong <itmonero@gmail.com>
2025-03-23 11:43:54 +0700

    Login page Version1

---
+++ remake-regTu/index.js
@@ -0,0 +1,6 @@
+const menu5 = document.querySelector(".menu5");
+const dropDown5 = document.querySelector(".dropDown5");
+
+menu5.addEventListener("click", function () {
+  dropDown5.classList.toggle("hidden");
+});
