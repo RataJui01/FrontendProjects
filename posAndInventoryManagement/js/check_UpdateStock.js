a0ddbefb968d4670bfe1d78b15b05373eb332bc2
RataJui01 <itmonero@gmail.com>
2025-05-02 12:02:48 +0700

    add click event to categories

--- posAndInventoryManagement/js/check_UpdateStock.js
+++ posAndInventoryManagement/js/check_UpdateStock.js
@@ -1,8 +1,20 @@
-window.addEventListener("scroll", function () {
-  const categoryContainer = document.querySelector(".categoryContainer");
-  if (window.scrollY >= 100) {
-    categoryContainer.classList.add("sticky");
-  } else {
-    categoryContainer.classList.remove("sticky");
-  }
+const categoryContainer = document.querySelector(".categoryContainer");
+const categoryArray = Array.from(categoryContainer.children);
+
+categoryArray.forEach(function (child, index) {
+  child.addEventListener("click", function () {
+    console.log(child);
+    for (const element of categoryArray.entries()) {
+      if (index === element[0]) {
+        continue;
+      } else {
+        if (element[1].classList.contains("clickCategoryContainer")) {
+          element[1].classList.remove("clickCategoryContainer");
+        } else {
+          continue;
+        }
+      }
+    }
+    child.classList.add("clickCategoryContainer");
+  });
 });
